79864f5ca59e96aedfd56919c822c07227ab0d69
Adran Carnavale <adran@hey.com>
2025-10-19 14:16:10 -0300

    bootstrap project

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("Counsil")
+	rootCmd := &cobra.Command{
+		Use:   "counsil",
+		Short: "Counsil CLI tool",
+		Long:  `A CLI tool for managing development tools.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Counsil CLI executed")
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+	}
 }
